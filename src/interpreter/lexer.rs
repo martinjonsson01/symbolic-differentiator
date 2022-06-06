0f304216eecddb600b68fea572450c3e113df9c5
Martin <martinjonsson01@gmail.com>
2022-06-06 16:10:36 +0200

    Implement basic lexer

--- src/interpreter/lexer.rs
+++ src/interpreter/lexer.rs
@@ -8,8 +8,20 @@ use super::token::Token;
 //           expression
 //        ))
 
+/// Turns the input string into individual tokens containing values
 pub fn tokenize(expression: String) -> Result<Vec<Token>> {
-    Ok([Token::Plus].to_vec())
+    let mut tokens: Vec<Token> = vec![];
+    for char in expression.chars() {
+        let token: Token = match char {
+            '+' => Token::Plus,
+            '-' => Token::Minus,
+            '*' => Token::Star,
+            '/' => Token::ForwardSlash,
+            name => Token::Identifier(name.to_string()),
+        };
+        tokens.push(token);
+    }
+    Ok(tokens)
 }
 
 #[cfg(test)]
@@ -18,13 +30,32 @@ mod tests {
     use pretty_assertions::{assert_eq, assert_ne};
 
     #[test]
-    fn correct_expression_returns_tokens() {
+    fn simple_expression_returns_tokens() {
         let expected_tokens = [
             Token::Identifier("x".to_string()),
             Token::Plus,
             Token::Identifier("y".to_string()),
         ];
         let actual_tokens = tokenize("x+y".to_string()).unwrap();
+        assert_eq!(actual_tokens, expected_tokens);
+    }
+
+    #[test]
+    fn long_expression_returns_tokens() {
+        let expected_tokens = [
+            Token::Identifier("a".to_string()),
+            Token::Plus,
+            Token::Identifier("b".to_string()),
+            Token::Minus,
+            Token::Identifier("c".to_string()),
+            Token::ForwardSlash,
+            Token::Identifier("d".to_string()),
+            Token::Star,
+            Token::Identifier("e".to_string()),
+            Token::Plus,
+            Token::Identifier("f".to_string()),
+        ];
+        let actual_tokens = tokenize("a+b-c/d*e+f".to_string()).unwrap();
         assert_eq!(actual_tokens, expected_tokens);
     }
 }

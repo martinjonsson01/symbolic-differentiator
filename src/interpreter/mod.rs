b482ba6b8263e95f6de7473ee32b1d4bd28b7553
Martin <martinjonsson01@gmail.com>
2022-08-03 16:34:11 +0200

    Interpret input text using lexer and parser

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,12 +1,29 @@
 pub mod lexer;
+mod operator;
 pub mod parser;
 mod token;
-mod operator;
 
-use anyhow::{Context, Result};
+use crate::interpreter::parser::expression_tree::ExpressionTree;
+use crate::interpreter::token::Token;
+use anyhow::Result;
 
-pub fn convert(expression: String) -> Result<String> {
-    let value: Vec<token::Token> = lexer::tokenize(expression)
-        .with_context(|| "could not parse expression".to_string())?;
-    Ok(format!("{:?}", value))
+/// Converts the given input string into an equivalent expression tree,
+/// which is easier to manipulate than the original string.
+///
+/// # Arguments
+///
+/// * `expression`: The text-representation of the infix expression.
+///
+/// returns: The equivalent expression tree.
+///
+/// # Examples
+///
+/// ```
+/// let tree = convert(expression.into())?;
+/// let regenerated_tokens = tree.to_infix();
+/// ```
+pub fn convert(expression: String) -> Result<ExpressionTree> {
+    let tokens = lexer::tokenize(expression)?;
+    let expression_tree = parser::parse(tokens)?;
+    Ok(expression_tree)
 }

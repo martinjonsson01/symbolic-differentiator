598a392c438974bc0ce401a8f926358107e7e9e5
Martin <martinjonsson01@gmail.com>
2022-08-03 15:59:45 +0200

    Implement expression regeneration from tree

--- src/interpreter/parser/expression_tree.rs
+++ src/interpreter/parser/expression_tree.rs
@@ -18,7 +18,7 @@ pub enum TraverseOrder {
 
 #[derive(PartialEq, Debug)]
 struct TokenNode {
-    value: Option<Token>,
+    value: Token,
     left: Option<Rc<TokenNode>>,
     right: Option<Rc<TokenNode>>,
 }
@@ -75,28 +75,72 @@ impl ExpressionTree {
     where
         F: FnMut(&Token),
     {
-        match &node.value {
-            None => return,
-            Some(value) => {
-                if order == TraverseOrder::PreOrder {
-                    (wrapper.function)(&value);
-                }
-                match &node.left {
-                    None => {}
-                    Some(left_node) => Self::traverse_nodes(&left_node, order, wrapper),
-                };
-                if order == TraverseOrder::InOrder {
-                    (wrapper.function)(&value);
-                }
-                match &node.right {
-                    None => {}
-                    Some(right_node) => Self::traverse_nodes(&right_node, order, wrapper),
-                };
-                if order == TraverseOrder::PostOrder {
-                    (wrapper.function)(&value);
+        if order == TraverseOrder::PreOrder {
+            (wrapper.function)(&node.value);
+        }
+        match &node.left {
+            None => {}
+            Some(left_node) => Self::traverse_nodes(&left_node, order, wrapper),
+        };
+        if order == TraverseOrder::InOrder {
+            (wrapper.function)(&node.value);
+        }
+        match &node.right {
+            None => {}
+            Some(right_node) => Self::traverse_nodes(&right_node, order, wrapper),
+        };
+        if order == TraverseOrder::PostOrder {
+            (wrapper.function)(&node.value);
+        }
+    }
+
+    pub fn to_infix(&self) -> Vec<Token> {
+        Self::build_expression(&self.root)
+    }
+
+    fn build_expression(node: &TokenNode) -> Vec<Token> {
+        let mut tokens: Vec<Token> = Vec::new();
+
+        if let Some(left_node) = &node.left {
+            let mut subtree_tokens = Self::build_expression_subtree(&node, &left_node);
+            tokens.append(&mut subtree_tokens);
+        }
+        
+        tokens.push(node.value.clone());
+
+        if let Some(right_node) = &node.right {
+            let mut subtree_tokens = Self::build_expression_subtree(&node, &right_node);
+            tokens.append(&mut subtree_tokens);
+        }
+        
+        tokens
+    }
+
+    fn build_expression_subtree(node: &TokenNode, left_node: &TokenNode) -> Vec<Token> {
+        let mut subtree_tokens: Vec<Token> = Vec::new();
+        if let Token::Operator(other_operator) = &left_node.value {
+            let mut close_parentheses = false;
+            if let Token::Operator(operator) = &node.value {
+                // When a child operator has lower precedence, it and its operands needs
+                // to be wrapped in parentheses.
+                if operator > other_operator {
+                    subtree_tokens.push(Token::OpenParenthesis);
+                    close_parentheses = true;
                 }
             }
-        };
+
+            // Recurse so that the entire subtree will be contained within 
+            // any potential parentheses.
+            let mut left_tokens = Self::build_expression(&left_node);
+            subtree_tokens.append(&mut left_tokens);
+
+            if close_parentheses {
+                subtree_tokens.push(Token::CloseParenthesis);
+            }
+        } else {
+            subtree_tokens.push(left_node.value.clone());
+        }
+        subtree_tokens
     }
 
     fn print(&self) -> io::Result<()> {
@@ -107,19 +151,15 @@ impl ExpressionTree {
 }
 
 fn write_node(node: &TokenNode, builder: &mut TreeBuilder) {
-    match &node.value {
-        None => return,
-        Some(value) => {
-            let node_name = format!("{}", value);
+    let node_name = format!("{}", &node.value);
 
-            if node.is_leaf() {
-                builder.add_empty_child(node_name);
-                return;
-            }
+    if node.is_leaf() {
+        builder.add_empty_child(node_name);
+        return;
+    }
 
-            builder.begin_child(node_name)
-        }
-    };
+    builder.begin_child(node_name);
+
     match &node.left {
         None => {}
         Some(left_node) => write_node(&left_node, builder),
@@ -134,7 +174,7 @@ fn write_node(node: &TokenNode, builder: &mut TreeBuilder) {
 impl TokenNode {
     fn new(token: Token) -> TokenNode {
         TokenNode {
-            value: Some(token),
+            value: token,
             left: None,
             right: None,
         }
@@ -159,7 +199,7 @@ mod tests {
 
     #[test]
     fn simple_expression_returns_correct_tree() {
-        let tokens = create_simple_tokens();
+        let tokens = create_simple_postfix_tokens();
         let expected_tree = create_simple_tree();
 
         let actual_tree = ExpressionTree::new(tokens).unwrap();
@@ -167,34 +207,9 @@ mod tests {
         assert_eq!(actual_tree, expected_tree);
     }
 
-    fn create_simple_tree() -> ExpressionTree {
-        let mut expected_tree = ExpressionTree {
-            root: TokenNode::new("+".parse().unwrap()),
-        };
-        expected_tree
-            .root
-            .set_left(TokenNode::new(Token::Identifier("x".into())));
-        expected_tree
-            .root
-            .set_right(TokenNode::new(Token::Identifier("y".into())));
-        let expected_tree = expected_tree;
-        expected_tree
-    }
-
-    fn create_simple_tokens() -> Vec<Token> {
-        // x + y (but in postfix notation)
-        let tokens = [
-            Token::Identifier("x".to_string()),
-            Token::Identifier("y".to_string()),
-            "+".parse().unwrap(),
-        ]
-        .to_vec();
-        tokens
-    }
-
     #[test]
     fn complex_expression_returns_correct_tree() {
-        let tokens = create_complex_tokens();
+        let tokens = create_complex_postfix_tokens();
         let expected_tree = create_complex_tree();
 
         let actual_tree = ExpressionTree::new(tokens).unwrap();
@@ -272,7 +287,63 @@ mod tests {
         assert_eq!(actual_order, expected_order);
     }
 
-    fn create_complex_tokens() -> Vec<Token> {
+    #[test]
+    fn simple_tree_converts_back_to_simple_expression() {
+        let expected_tokens = create_simple_infix_tokens();
+        let tree = create_simple_tree();
+
+        let actual_tokens = tree.to_infix();
+
+        assert_eq!(actual_tokens, expected_tokens);
+    }
+
+    #[test]
+    fn complex_tree_converts_back_to_simple_expression() {
+        let expected_tokens = create_complex_infix_tokens();
+        let tree = create_complex_tree();
+
+        let actual_tokens = tree.to_infix();
+
+        assert_eq!(actual_tokens, expected_tokens);
+    }
+
+    fn create_simple_tree() -> ExpressionTree {
+        let mut expected_tree = ExpressionTree {
+            root: TokenNode::new("+".parse().unwrap()),
+        };
+        expected_tree
+            .root
+            .set_left(TokenNode::new(Token::Identifier("x".into())));
+        expected_tree
+            .root
+            .set_right(TokenNode::new(Token::Identifier("y".into())));
+        let expected_tree = expected_tree;
+        expected_tree
+    }
+
+    fn create_simple_postfix_tokens() -> Vec<Token> {
+        // x + y (but in postfix notation)
+        let tokens = [
+            Token::Identifier("x".to_string()),
+            Token::Identifier("y".to_string()),
+            "+".parse().unwrap(),
+        ]
+        .to_vec();
+        tokens
+    }
+
+    fn create_simple_infix_tokens() -> Vec<Token> {
+        // x + y
+        let tokens = [
+            Token::Identifier("x".to_string()),
+            "+".parse().unwrap(),
+            Token::Identifier("y".to_string()),
+        ]
+        .to_vec();
+        tokens
+    }
+
+    fn create_complex_postfix_tokens() -> Vec<Token> {
         // x + ((y + z) * a) (but in postfix notation)
         [
             Token::Identifier("x".to_string()),
@@ -282,6 +353,22 @@ mod tests {
             Token::Identifier("a".to_string()),
             "*".parse().unwrap(),
             "+".parse().unwrap(),
+        ]
+        .to_vec()
+    }
+
+    fn create_complex_infix_tokens() -> Vec<Token> {
+        // x + (y + z) * a
+        [
+            Token::Identifier("x".to_string()),
+            "+".parse().unwrap(),
+            Token::OpenParenthesis,
+            Token::Identifier("y".to_string()),
+            "+".parse().unwrap(),
+            Token::Identifier("z".to_string()),
+            Token::CloseParenthesis,
+            "*".parse().unwrap(),
+            Token::Identifier("a".to_string()),
         ]
         .to_vec()
     }

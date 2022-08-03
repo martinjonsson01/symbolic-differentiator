ae61f58f3a3513412e619e91e510c2b9fd3f2f84
Martin <martinjonsson01@gmail.com>
2022-08-03 16:51:40 +0200

    Implement token vector pretty-printing

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -5,7 +5,8 @@ mod token;
 
 use crate::interpreter::parser::expression_tree::ExpressionTree;
 use crate::interpreter::token::Token;
-use anyhow::Result;
+use anyhow::{Context, Result};
+use string_builder::Builder;
 
 /// Converts the given input string into an equivalent expression tree,
 /// which is easier to manipulate than the original string.
@@ -26,4 +27,36 @@ pub fn convert(expression: String) -> Result<ExpressionTree> {
     let tokens = lexer::tokenize(expression)?;
     let expression_tree = parser::parse(tokens)?;
     Ok(expression_tree)
+}
+
+/// Pretty-prints the given vector of tokens with added whitespace.
+///
+/// # Arguments
+///
+/// * `tokens`: The tokens to print.
+///
+/// returns: A pretty-printed text-version of the given tokens.
+///
+/// # Examples
+///
+/// ```
+/// let pretty_printed_tokens = tokens_to_string(tokens);
+/// print!("{}", pretty_printed_tokens);
+/// ```
+pub fn tokens_to_string(tokens: Vec<Token>) -> Result<String> {
+    let mut builder = Builder::new(tokens.len());
+    
+    for token in tokens {
+        match token {
+            Token::Literal(value) => builder.append(format!("{:.2}", value)),
+            Token::Operator(operator) => {
+                builder.append(" ");
+                builder.append(operator.to_string());
+                builder.append(" ");
+            }
+            _ => builder.append(token.to_string()),
+        }
+    }
+    
+    builder.string().context("Failed to build token string")
 }
